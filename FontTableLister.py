542e223b73b2a4500f1d3ea7bbb72d7bc4fdb5b6
Eric Mader <emader@gmx.us>
2020-01-17 09:55:26 -1000

    Add font module for file, table access.

--- FontTableLister.py
+++ FontTableLister.py
@@ -7,31 +7,18 @@ Created on Jan 16, 2020
 import struct
 import sys
 
-def stringFromTag(tag):
-    # There's *got* to be a better way to do this...
-    return "%c%c%c%c" % (tag[0], tag[1], tag[2], tag[3])
+import font
 
-fontDirectoryHeaderFormat = ">IHHHH"
-fontDirectoryEntryFormat  = ">4sIII"
-
-fontDirectoryHeaderLength = struct.calcsize(fontDirectoryHeaderFormat)
-fontDirectoryEntryLength  = struct.calcsize(fontDirectoryEntryFormat)
 
 for fontFileName in sys.argv[1:] :
-    fontFile = open(fontFileName, "rb")
+    fontFile = font.Font(fontFileName)
     
     fontName = fontFileName.split("/")[-1:][0] # [-1:] gives a list with 1 string in it, so need [0] to get the string
     
-    directoryHeaderData = fontFile.read(fontDirectoryHeaderLength)
-    scalerType, numTables, searchRange, entrySelector, rangeShift = struct.unpack(fontDirectoryHeaderFormat, directoryHeaderData)
+    print(fontName + " contains " + str(fontFile.numTables) + " tables.")
     
-    print(fontName + " contains " + str(numTables) + " tables.")
-    
-    for entry in range(numTables) :
-        directoryEntryData = fontFile.read(fontDirectoryEntryLength)
-        tag, checksum, offset, length = struct.unpack(fontDirectoryEntryFormat, directoryEntryData)
-        
-        print("'" + stringFromTag(tag) + "'" + " " + str(length))
+    for table in fontFile.tables :
+        print("'" + table.tag_string() + "'" + " " + str(table.length))
     
     print()
     
---
+++ font.py
@@ -0,0 +1,47 @@
+'''
+Created on Jan 17, 2020
+
+@author: emader
+'''
+
+import sys
+import struct
+import string
+
+class Font(object):
+    '''
+    classdocs
+    '''
+    FONT_DIRECTORY_HEADER_FORMAT = ">IHHHH"
+
+    FONT_DIRECTORY_HEADER_LENGTH = struct.calcsize(FONT_DIRECTORY_HEADER_FORMAT)
+
+
+    def __init__(self, filePath):
+        self.fontFile = open(filePath, "rb")
+        #fileName = filePath.split("/")[-1:][0] # [-1:] gives a list with 1 string in it, so need [0] to get the string
+        directoryHeaderData = self.fontFile.read(self.FONT_DIRECTORY_HEADER_LENGTH)
+        self.scalerType, self.numTables, self.searchRange, self.entrySelector, self.rangeShift = struct.unpack(self.FONT_DIRECTORY_HEADER_FORMAT, directoryHeaderData)
+
+        self.tables = []
+        for table in range(self.numTables) :
+            self.tables.append(Table(self.fontFile))
+            
+            
+class Table(object):
+    '''
+    classdocs
+    '''
+    FONT_DIRECTORY_ENTRY_FORMAT  = ">4sIII"
+    FONT_DIRECTORY_ENTRY_LENGTH  = struct.calcsize(FONT_DIRECTORY_ENTRY_FORMAT)
+
+    def __init__(self, fontFile):
+        self.fontFile = fontFile
+        
+        directoryEntryData = fontFile.read(self.FONT_DIRECTORY_ENTRY_LENGTH)
+        self.tag, self.checksum, self.offset, self.length = struct.unpack(self.FONT_DIRECTORY_ENTRY_FORMAT, directoryEntryData)
+
+    def tag_string(self) -> string:
+        # There's *got* to be a better way to do this...
+        return "%c%c%c%c" % (self.tag[0], self.tag[1], self.tag[2], self.tag[3])
+
