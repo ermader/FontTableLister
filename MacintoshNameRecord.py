5f4f2ae497945fe655bb02832bdea2146b1fca58
Eric Mader <emader@gmx.us>
2020-02-04 14:03:51 -1000

    Macintosh language names.

--- MacintoshNameRecord.py
+++ MacintoshNameRecord.py
@@ -40,6 +40,56 @@ class MacintoshNameRecord(NameRecord.NameRecord):
     ENCODING_ID_VIETNAMESE = 30
     ENCODING_ID_SINDHI = 31
 
+    LANGUAGE_ID_ENGLISH = 0
+    LANGUAGE_ID_FRENCH = 1
+    LANGUAGE_ID_GERMAN = 2
+    LANGUAGE_ID_ITALIAN = 3
+    LANGUAGE_ID_DUCTH = 4
+    LANGUAGE_ID_SWEDISH = 5
+    LANGUAGE_ID_SPANISH = 6
+    LANGUAGE_ID_DANISH = 7
+    LANGUAGE_ID_PORTUGUESE = 8
+    LANGUAGE_ID_NORWEGIAN = 9
+    LANGUAGE_ID_HEBREW = 10
+    LANGUAGE_ID_JAPANESE = 11
+    LANGUAGE_ID_ARABIC = 12
+    LANGUAGE_ID_FINNISH = 13
+    LANGUAGE_ID_GREEK = 14
+    LANGUAGE_ID_ICELANDIC = 15
+    LANGUAGE_ID_MALTESE = 16
+    LANGUAGE_ID_TURKISH = 17
+    LANGUAGE_ID_CROATIAN = 18
+    LANGUAGE_ID_CHINESE_TRADITIONAL = 19
+    LANGUAGE_ID_URDU = 20
+    LANGUAGE_ID_HINDI = 21
+    LANGUAGE_ID_THAI = 22
+    LANGUAGE_ID_KOREAN = 23
+    LANGUAGE_ID_LITHUANIAN = 24
+    LANGUAGE_ID_POLISH = 25
+    LANGUAGE_ID_HUNGARIAN = 26
+    LANGUAGE_ID_ESTONIAN = 27
+    LANGUAGE_ID_LATVIAN = 28
+    LANGUAGE_ID_SAMI = 29
+    LANGUAGE_ID_FAROESE = 30
+    LANGUAGE_ID_FARSI = 31
+    LANGUAGE_ID_RUSSIAN = 32
+    LANGUAGE_ID_CHINESE_SIMPLIFIED = 33
+    LANGUAGE_ID_FLEMISH = 34
+    LANGUAGE_ID_IRISH_GAELIC = 35
+    LANGUAGE_ID_ALBANIAN = 36
+    LANGUAGE_ID_ROMANIAN = 37
+    LANGUAGE_ID_CZECH = 38
+    LANGUAGE_ID_SLOVAK = 39
+    LANGUAGE_ID_SLOVENIAN = 40
+    LANGUAGE_ID_YIDDISH = 41
+    LANGUAGE_ID_SERBIAN = 42
+    LANGUAGE_ID_MACEDONIAN = 43
+    LANGUAGE_ID_BULGARIAN = 44
+    LANGUAGE_ID_UKRANIAN = 45
+    LANGUAGE_ID_BYELORUSSIAN = 46
+    LANGUAGE_ID_CATALAN = 130
+    # and a bunch more...
+
     def __init__(self, platformID, encodingID, languageID, nameID, length, offset):
         NameRecord.NameRecord.__init__(self, platformID, encodingID, languageID, nameID, length, offset)
 
@@ -78,6 +128,56 @@ class MacintoshNameRecord(NameRecord.NameRecord):
             self.ENCODING_ID_SINDHI : "Sindhi"
         }
 
+        self.languageNames = {
+            self.LANGUAGE_ID_ENGLISH : "English",
+            self.LANGUAGE_ID_FRENCH : "French",
+            self.LANGUAGE_ID_GERMAN : "German",
+            self.LANGUAGE_ID_ITALIAN : "Italian",
+            self.LANGUAGE_ID_DUCTH : "Dutch",
+            self.LANGUAGE_ID_SWEDISH : "Swedish",
+            self.LANGUAGE_ID_SPANISH : "Spanish",
+            self.LANGUAGE_ID_DANISH : "Danish",
+            self.LANGUAGE_ID_PORTUGUESE : "Portuguese",
+            self.LANGUAGE_ID_NORWEGIAN : "Norwegian",
+            self.LANGUAGE_ID_HEBREW : "Hebrew",
+            self.LANGUAGE_ID_JAPANESE : "Japanese",
+            self.LANGUAGE_ID_ARABIC : "Arabic",
+            self.LANGUAGE_ID_FINNISH : "Finish",
+            self.LANGUAGE_ID_GREEK : "Greek",
+            self.LANGUAGE_ID_ICELANDIC : "Icelandic",
+            self.LANGUAGE_ID_MALTESE : "Maltese",
+            self.LANGUAGE_ID_TURKISH : "Turkish",
+            self.LANGUAGE_ID_CROATIAN : "Croation",
+            self.LANGUAGE_ID_CHINESE_TRADITIONAL : "Chinese (Traditional)",
+            self.LANGUAGE_ID_URDU : "Urdu",
+            self.LANGUAGE_ID_HINDI : "Hindi",
+            self.LANGUAGE_ID_THAI : "Thai",
+            self.LANGUAGE_ID_KOREAN : "Korean",
+            self.LANGUAGE_ID_LITHUANIAN : "Lithuanian",
+            self.LANGUAGE_ID_POLISH : "Polish",
+            self.LANGUAGE_ID_HUNGARIAN : "Hungarian",
+            self.LANGUAGE_ID_ESTONIAN : "Estonian",
+            self.LANGUAGE_ID_LATVIAN : "Latvian",
+            self.LANGUAGE_ID_SAMI : "Sami",
+            self.LANGUAGE_ID_FAROESE : "Faroese",
+            self.LANGUAGE_ID_FARSI : "Farsi",
+            self.LANGUAGE_ID_RUSSIAN : "Russian",
+            self.LANGUAGE_ID_CHINESE_SIMPLIFIED : "Chinese (Simplified)",
+            self.LANGUAGE_ID_FLEMISH : "Flemish",
+            self.LANGUAGE_ID_IRISH_GAELIC : "Irish Gaelic",
+            self.LANGUAGE_ID_ALBANIAN : "Albanian",
+            self.LANGUAGE_ID_ROMANIAN : "Romanian",
+            self.LANGUAGE_ID_CZECH : "Czech",
+            self.LANGUAGE_ID_SLOVAK : "Slovak",
+            self.LANGUAGE_ID_SLOVENIAN : "Slovenian",
+            self.LANGUAGE_ID_YIDDISH : "Yiddish",
+            self.LANGUAGE_ID_SERBIAN : "Serbian",
+            self.LANGUAGE_ID_MACEDONIAN : "Macedonian",
+            self.LANGUAGE_ID_BULGARIAN : "Bulgarian",
+            self.LANGUAGE_ID_UKRANIAN : "Ukkranian",
+            self.LANGUAGE_ID_BYELORUSSIAN : "Byelorussian",
+            self.LANGUAGE_ID_CATALAN : "Catalan"
+        }
 
     def platformName(self):
         return "Macintosh"
