1172e789f98b7b929ad69643939798e48724e8d5
Eric Mader <emader@gmx.us>
2020-02-07 10:03:37 -1000

    Clean up Macintosh string encodings a bit. Many not supported by Python?

--- MacintoshNameRecord.py
+++ MacintoshNameRecord.py
@@ -132,38 +132,37 @@ class MacintoshNameRecord(NameRecord.NameRecord):
         # Many of these don't seem to have Python codecs...
         #
         self.stringEncodings = {
-            self.ENCODING_ID_ROMAN: "*",
             self.ENCODING_ID_JAPANESE: "shift_jis", # MacJapanese is actually a superset of SHIFT-JIS
-            self.ENCODING_ID_CHINESE_TRADITIONAL: "Chinese (Traditional)",
-            self.ENCODING_ID_KOREAN: "euc_kr", # MacKoran is acutally a superset of EUC-KR
-            self.ENCODING_ID_ARABIC: "Arabic",
-            self.ENCODING_ID_HEBREW: "Hebrew",
+            # self.ENCODING_ID_CHINESE_TRADITIONAL: "Chinese (Traditional)",
+            self.ENCODING_ID_KOREAN: "euc_kr", # MacKoran is actually a superset of EUC-KR
+            # self.ENCODING_ID_ARABIC: "Arabic",
+            # self.ENCODING_ID_HEBREW: "Hebrew",
             self.ENCODING_ID_GREEK: "mac_greek",
             self.ENCODING_ID_RUSSIAN: "mac_cyrillic",
             self.ENCODING_ID_RSYMBOL: "koi8_r", # not sure about this...
-            self.ENCODING_ID_DEVANAGARI: "Devanagari",
-            self.ENCODING_ID_GURMUKHI: "Gurmukhi",
-            self.ENCODING_ID_GUJARATI: "Gujarati",
-            self.ENCODING_ID_ORIYA: "Oriya",
-            self.ENCODING_ID_BENGALI: "Bengali",
-            self.ENCODING_ID_TAMIL: "Tamil",
-            self.ENCODING_ID_TELEGU: "Telegu",
-            self.ENCODING_ID_KANNADA: "Kannada",
-            self.ENCODING_ID_MALAYALAM: "Malayalam",
-            self.ENCODING_ID_SINHALESE: "Sinhalese",
-            self.ENCODING_ID_BURMESE: "Burmese",
-            self.ENCODING_ID_KHMER: "Khmer",
-            self.ENCODING_ID_THAI: "Thai",
-            self.ENCODING_ID_LAO: "Lao",
-            self.ENCODING_ID_GEORGIAN: "Georgian",
-            self.ENCODING_ID_ARMENIAN: "Armenian",
-            self.ENCODING_ID_CHINESE_SIMPLIFIED: "Chinese (Simplified)",
-            self.ENCODING_ID_TIBETAN: "Tibetan",
-            self.ENCODING_ID_MONGOLIAN: "Mongolian",
-            self.ENCODING_ID_GEEZ: "Geez",
-            self.ENCODING_ID_SLAVIC: "Slavic",
-            self.ENCODING_ID_VIETNAMESE: "Vietnamese",
-            self.ENCODING_ID_SINDHI: "Sindhi"
+            # self.ENCODING_ID_DEVANAGARI: "Devanagari",
+            # self.ENCODING_ID_GURMUKHI: "Gurmukhi",
+            # self.ENCODING_ID_GUJARATI: "Gujarati",
+            # self.ENCODING_ID_ORIYA: "Oriya",
+            # self.ENCODING_ID_BENGALI: "Bengali",
+            # self.ENCODING_ID_TAMIL: "Tamil",
+            # self.ENCODING_ID_TELEGU: "Telegu",
+            # self.ENCODING_ID_KANNADA: "Kannada",
+            # self.ENCODING_ID_MALAYALAM: "Malayalam",
+            # self.ENCODING_ID_SINHALESE: "Sinhalese",
+            # self.ENCODING_ID_BURMESE: "Burmese",
+            # self.ENCODING_ID_KHMER: "Khmer",
+            # self.ENCODING_ID_THAI: "Thai",
+            # self.ENCODING_ID_LAO: "Lao",
+            # self.ENCODING_ID_GEORGIAN: "Georgian",
+            # self.ENCODING_ID_ARMENIAN: "Armenian",
+            # self.ENCODING_ID_CHINESE_SIMPLIFIED: "Chinese (Simplified)",
+            # self.ENCODING_ID_TIBETAN: "Tibetan",
+            # self.ENCODING_ID_MONGOLIAN: "Mongolian",
+            # self.ENCODING_ID_GEEZ: "Geez",
+            self.ENCODING_ID_SLAVIC: "mac_latin2",
+            # self.ENCODING_ID_VIETNAMESE: "Vietnamese",
+            # self.ENCODING_ID_SINDHI: "Sindhi"
         }
 
         self.languageNames = {
@@ -217,8 +216,64 @@ class MacintoshNameRecord(NameRecord.NameRecord):
             self.LANGUAGE_ID_CATALAN : "Catalan"
         }
 
+        self.languageEncodings = {
+            self.LANGUAGE_ID_ENGLISH: "mac_roman",
+            self.LANGUAGE_ID_FRENCH: "mac_roman",
+            self.LANGUAGE_ID_GERMAN: "mac_roman",
+            self.LANGUAGE_ID_ITALIAN: "mac_roman",
+            self.LANGUAGE_ID_DUCTH: "mac_roman",
+            self.LANGUAGE_ID_SWEDISH: "mac_roman",
+            self.LANGUAGE_ID_SPANISH: "mac_roman",
+            self.LANGUAGE_ID_DANISH: "mac_roman",
+            self.LANGUAGE_ID_PORTUGUESE: "mac_roman",
+            self.LANGUAGE_ID_NORWEGIAN: "mac_roman",
+            # self.LANGUAGE_ID_HEBREW: "Hebrew",
+            # self.LANGUAGE_ID_JAPANESE: "Japanese",
+            # self.LANGUAGE_ID_ARABIC: "Arabic",
+            self.LANGUAGE_ID_FINNISH: "mac_roman",
+            self.LANGUAGE_ID_GREEK: "mac_greek",
+            self.LANGUAGE_ID_ICELANDIC: "mac_iceland",
+            self.LANGUAGE_ID_MALTESE: "mac_roman",
+            self.LANGUAGE_ID_TURKISH: "mac_turkish",
+            # self.LANGUAGE_ID_CROATIAN: "Croation",
+            # self.LANGUAGE_ID_CHINESE_TRADITIONAL: "Chinese (Traditional)",
+            # self.LANGUAGE_ID_URDU: "Urdu",
+            # self.LANGUAGE_ID_HINDI: "Hindi",
+            # self.LANGUAGE_ID_THAI: "Thai",
+            # self.LANGUAGE_ID_KOREAN: "Korean",
+            self.LANGUAGE_ID_LITHUANIAN: "mac_latin2",
+            self.LANGUAGE_ID_POLISH: "mac_latin2",
+            self.LANGUAGE_ID_HUNGARIAN: "mac_latin2",
+            self.LANGUAGE_ID_ESTONIAN: "mac_latin2",
+            self.LANGUAGE_ID_LATVIAN: "mac_latin2",
+            self.LANGUAGE_ID_SAMI: "mac_roman",
+            self.LANGUAGE_ID_FAROESE: "mac_roman",
+            # self.LANGUAGE_ID_FARSI: "Farsi",
+            self.LANGUAGE_ID_RUSSIAN: "mac_cyrillic",
+            self.LANGUAGE_ID_CHINESE_SIMPLIFIED: "Chinese (Simplified)",
+            self.LANGUAGE_ID_FLEMISH: "mac_roman",
+            self.LANGUAGE_ID_IRISH_GAELIC: "mac_roman Gaelic",
+            self.LANGUAGE_ID_ALBANIAN: "mac_latin2",
+            # self.LANGUAGE_ID_ROMANIAN: "Romanian",
+            self.LANGUAGE_ID_CZECH: "mac_latin2",
+            self.LANGUAGE_ID_SLOVAK: "mac_latin2",
+            self.LANGUAGE_ID_SLOVENIAN: "mac_latin2",
+            self.LANGUAGE_ID_YIDDISH: "mac_roman",
+            self.LANGUAGE_ID_SERBIAN: "mac_cyrillic",
+            self.LANGUAGE_ID_MACEDONIAN: "mac_cyrillic",
+            self.LANGUAGE_ID_BULGARIAN: "mac_cyrillic",
+            self.LANGUAGE_ID_UKRANIAN: "mac_cyrillic",
+            self.LANGUAGE_ID_BYELORUSSIAN: "mac_cyrillic",
+            self.LANGUAGE_ID_CATALAN: "mac_roman"
+        }
+
     def platformName(self):
         return "Macintosh"
 
     def getStringEncoding(self):
-        return "mac_roman" # A good default for the cases I'm seeing
+        if self.encodingID == self.ENCODING_ID_JAPANESE:
+            encoding = self.languageEncodings.get(self.languageID)
+        else:
+            encoding = self.stringEncodings.get(self.encodingID)
+
+        return encoding
