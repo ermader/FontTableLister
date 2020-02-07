1029836b6ef41fec2c67ab7c354f2d071c76e1e3
Eric Mader <emader@gmx.us>
2020-02-07 09:30:30 -1000

    Select font based on postscript name.

--- FontTableLister.py
+++ FontTableLister.py
@@ -15,6 +15,7 @@ parser = argparse.ArgumentParser(prog="FontTableLister")
 parser.add_argument("file", type=argparse.FileType("rb"), help="the font file to examine")
 parser.add_argument("-c", "--contents", action="store_true", help="List all fonts in the file")
 parser.add_argument("-i", "--index", type=int, default=0, metavar="font_index", help="the subfont to examine. (default = %(default)s)")
+parser.add_argument("-p", "--psname", metavar="postscript_name", help="postscript name of the font to examine")
 parser.add_argument("-d", "--dump", action="append", metavar="table", help="hex dump the table")
 parser.add_argument("-f", "--format", action="append", metavar="table", help="show the formatted table")
 parser.add_argument("-l", "--list", action="store_true", help="list all the tables in the font")
@@ -24,7 +25,6 @@ arguments = parser.parse_args(sys.argv[1:])
 fileObject = arguments.file
 fileName = fileObject.name.split(os.sep)[-1:][0] # [-1:] gives a list with 1 string in it, so need [0] to get the string
 
-print(f"{fileName}:")
 fontFile = FontFile.File(fileObject)
 
 if arguments.contents:
@@ -36,8 +36,24 @@ if arguments.contents:
 
     print()
 
-fontIndex = min(len(fontFile.fonts) - 1, arguments.index)
+fontIndex = None
+if arguments.psname:
+    index = 0
+    for fontObject in fontFile.fonts:
+        if fontObject.getPostscriptName() == arguments.psname:
+            fontIndex = index
+            break
+        index += 1
+
+    if fontIndex is None:
+        print(f"  {fileName} does not contain a font named {arguments.psname}")
+
+if fontIndex is None:
+    fontIndex = min(len(fontFile.fonts) - 1, arguments.index)
+
 fontObject = fontFile.fonts[fontIndex]
+
+print(f"{fileName}/{fontObject.getPostscriptName()}:")
 
 if arguments.list:
     print(f"  {fontObject.getPostscriptName()} contains {len(fontObject.tables):d} tables:")
